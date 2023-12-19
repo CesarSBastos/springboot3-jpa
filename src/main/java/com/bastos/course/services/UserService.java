2489c8008da83a4188c9cc248d9624ff63c4a071
CesarSBastos <cesarbastos32@gmail.com>
2023-12-19 11:08:20 -0300

    Service layer, componnent registration

---
+++ src/main/java/com/bastos/course/services/UserService.java
@@ -0,0 +1,23 @@
+package com.bastos.course.services;
+
+import com.bastos.course.entities.User;
+import com.bastos.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
